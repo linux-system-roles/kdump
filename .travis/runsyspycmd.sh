b706562a7cd2977932c8f60fdf5cebf0f0be669c
Rich Megginson <rmeggins@redhat.com>
2020-12-09 16:54:14 -0700

    remove trace

--- .travis/runsyspycmd.sh
+++ .travis/runsyspycmd.sh
@@ -8,7 +8,7 @@
 #   $1     - command runnable in Python (should be present in $PATH)
 #   ${@:2} - arguments passed to $1
 
-set -ex
+set -e
 
 ME=$(basename "$0")
 SCRIPTDIR=$(readlink -f "$(dirname "$0")")
